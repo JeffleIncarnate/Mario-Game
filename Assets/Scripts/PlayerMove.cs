2f2c1421c2d240bf9bee321b68d8b561035b8a2f
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-01-21 11:42:02 +1300

    Damn

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Rigidbody2D rb2D;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Player has pressed up");
+        }
+
+        if(Input.GetKey ("down"))
+        {
+            Debug.Log("Player has pressed down");
+        }
+    }
+}
