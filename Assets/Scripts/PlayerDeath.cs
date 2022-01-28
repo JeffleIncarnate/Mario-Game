f2cc9fe9e6a60fae63cc4543241724859cb85f1b
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-01-29 11:56:47 +1300

    Added a death Script, as well as animations

---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    float deathY = -20f;
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < deathY)
+        {
+            Debug.Log("You have died");
+            animator.SetBool("isJumping", false);
+            animator.SetBool("isDead", true);
+        }
+    }
+}
